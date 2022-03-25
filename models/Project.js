6659e40c34702f61126ce67413f0b8ee02893d5a
Rica5 <97278047+Rica5@users.noreply.github.com>
2022-03-25 19:36:46 +0300

    Update Project.js

--- models/Project.js
+++ models/Project.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 const Project = mongoose.Schema({
     project_name:String,
-    status:String
+    parent:parent
 })
 module.exports = mongoose.model('Project',Project);
